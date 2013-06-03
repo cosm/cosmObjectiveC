50e2df41f6eb50d994b515b502050e079387c01a
Fraser Macdonald <macdonaldfk@gmail.com>
2013-06-03 12:18:34 +0100

    point to Xively

--- SocketFeedExampleiOS/SocketFeedExampleiOS/DatastreamCell.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface DatastreamCell : UITableViewCell
-
-// Interface Builder
-@property (nonatomic, weak) IBOutlet UILabel *currentValueLabel;
-@property (nonatomic, weak) IBOutlet UILabel *datastreamNameLabel;
-
-@end
